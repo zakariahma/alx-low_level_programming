dee224850bc6d68d9973832641f177345bfcffa0
zakariahma <zakariahmamed@gmail.com>
2023-03-21 16:26:15 -0700

    task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,7 @@ void print_alphabet_x10(void)
 	for (j = 1; j <= 10; j++)
 	{
         	for (i = 97; i <= 122; i++)
-		{
                 	_putchar(i);
-        		_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
